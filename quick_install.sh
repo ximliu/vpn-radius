5e11d2fe8a715af004233ca4d2a5bf2bc738b896
Napoléon Bonaparte <ximliu0228aicmz@gmail.com>
2019-07-06 20:50:29 +0800

    Update quick_install.sh

--- quick_install.sh
+++ quick_install.sh
@@ -1,7 +1,7 @@
 #复制下面的所有代码，粘贴到你的服务器进行安装（你的服务器操作系统必须是Centos7）
 sudo -i
 yum install wget -y
-wget http://180.188.197.212/down/installvpn.sh
-chmod +x installvpn.sh
-./installvpn.sh
+https://raw.githubusercontent.com/ximliu/vpn-radius/master/install_script.sh
+chmod +x install_script.sh
+./install_script.sh
 #
